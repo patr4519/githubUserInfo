44c3c545ac310b3644ac3fd63036f91c1b08fcef
patr4519 <jero96@mail.ru>
2022-12-24 23:52:32 +0300

    NotFoundIMG

--- src/Elements/UnnownUser.jsx
+++ src/Elements/UnnownUser.jsx
@@ -1,23 +1,12 @@
 import React from "react";
+import NoSuchUser from '../img/0default.png'
 
 export const UnnownUser = () => {
-    // return (
-    //     <li>
-    //         <div className='item'>
-    //             <div className='userIMG'>
-    //                 <img src="https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg" alt="user" className="photo"></img>
-    //             </div>
-    //             <div className="userName">Not found</div>
-    //             <div className='userDescription'>...</div>
-    //             <button className="closeUser" type="button">x</button>
-    //         </div>
-    //     </li>
-    // );
     return (
         <li>
             <div className='item'>
                 <div className='userIMG'>
-                    <img src="https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg" alt="user" className="photo"></img>
+                    <img src={NoSuchUser} alt="user" className="photo"></img>
                 </div>
                 <div className="userName">Not found</div>
                 <div className='userDescription'>...</div>
