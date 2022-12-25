683b31c7ff2a759d38afb376314f072093abb5ee
patr4519 <jero96@mail.ru>
2022-12-25 18:35:48 +0300

    18:35

--- src/Functions/validationInput.js
+++ src/Functions/validationInput.js
@@ -1,18 +1,3 @@
-// export function validationInput(string) {
-//     function validation(str) {
-//         let pattern = /[A-Za-z0-9]+/g;
-//         return str.match(pattern)[0] === str;
-//     }
-
-//     let validArr = [];
-//     string = string.split(', ');
-//     string.map(item => {
-//         validArr.push(validation(item))
-//     });
-//     if (validArr.includes(false)) return false;
-//     else return true;
-// }
-
 export function validationInput(string) {
     function validation(str) {
         let pattern = /[A-Za-z0-9]+/g;
